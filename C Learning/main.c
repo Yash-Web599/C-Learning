69015a7de9e3d5b6b380e70ca02195f5975be1d1
Yash <yashm6711@gmail.com>
2025-09-22 19:48:41 +0530

    hi

--- C Learning/main.c
+++ C Learning/main.c
@@ -5,28 +5,22 @@
 //  Created by apple on 22/09/25.
 //
 
-#include <stdio.h>
-
-int main() {
-    int number;
-    
-    
-    printf("Enter Any number: ");
-    scanf("%d", &number);
-    
-    for (int i = 1; i <= number; i++) {
-        if (i % 3 ==0 && i % 5 == 0) {
-            printf("FizzBuzz\n");
-        } else if(i % 3 == 0){
-            printf("Fizz\n");
-        } else if (i % 5 == 0){
-            printf("Buzz\n");
-        }
-        else {
-            printf("%d\n",i);
-        }
-        
-    }
-    return 0;
-}
-
+//#include <stdio.h>
+//
+//int main () {
+//    int input;
+//    int sum = 0;
+//
+//    printf("Enter any number: ");
+//    scanf("%d", &input);
+//
+//    for (int i = 1; i<=input; i++) {
+//        if (i % 2 == 0) {
+//            sum -= i;
+//        }
+//        else {
+//            sum += i;
+//        }
+//    }
+//    printf("Sum = %d\n", sum);
+//}
---
+++ C Learning/task_4.c
@@ -0,0 +1,8 @@
+//
+//  task_4.c
+//  C Learning
+//
+//  Created by apple on 22/09/25.
+//
+
+#include <stdio.h>
