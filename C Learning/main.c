d6db87ac380982d300c5a3b01e137466cabb655b
Yash <yashm6711@gmail.com>
2025-09-22 19:03:40 +0530

    Initial Commit

---
+++ C Learning/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Learning
+//
+//  Created by apple on 22/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
