093fd2def3b3e3cde2fe8a9413d6104bd052229c
Yash <yashm6711@gmail.com>
2025-10-13 17:41:29 +0530

    [Feature] created a calculator

--- C Learning/practice3.c
+++ C Learning/practice3.c
@@ -1,24 +1,79 @@
-//
-//  practice3.c
-//  C Learning
-//
-//  Created by apple on 26/09/25.
-
-
 #include <stdio.h>
 
-    int sum (int* , int*);
 
-    int sum(int* x, int* y) {
-        *x = 4;
-        return (*x + *y);
-    }
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
 
 int main() {
+    int num1, num2, choice;
+    float result;
     
-    int a = 1 , b = 2;
-    printf("Sum of 1 and 2 : %d\n", sum(&a , &b));
-    printf("Value of a : %d\n", a);
+    
+    printf("Enter first numbers: ");
+    scanf("%d", &num1);
+    
+    printf("Enter second numbers: ");
+    scanf("%d", &num2);
+    
+    do {
+        printf("\n===== CALCULATOR =====\n");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch (choice) {
+            case 1:
+                result = add(num1, num2);
+                printf("Result: %d\n", add(num1, num2));
+                break;
+            case 2:
+                result = subtract(num1, num2);
+                printf("Result: %d\n", subtract(num1, num2));
+                break;
+            case 3:
+                result = multiply(num1, num2);
+                printf("Result: %d\n", multiply(num1, num2));
+                break;
+            case 4:
+                result = divide(num1, num2);
+                if (num2 != 0)
+                    printf("Result: %.2f\n", result);
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice!\n");
+        }
+    } while (choice != 5);
     
     return 0;
+}
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(int a, int b) {
+    if (b != 0)
+        return (float)a / b;
+    else {
+        printf("Division by zero is not allowed.\n");
+        return 0;
+    }
 }
