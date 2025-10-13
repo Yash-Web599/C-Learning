c3f1d589a15525d493ba73cc364341f9fc3fa68c
Yash <yashm6711@gmail.com>
2025-10-13 18:06:26 +0530

    [Clean] rearrange the prototype function

--- C Learning/practice3.c
+++ C Learning/practice3.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
 
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-float divide(int a, int b);
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    float divide(int a, int b) {
+        if (b != 0)
+            return (float)a / b;
+        else {
+            printf("Division by zero is not allowed.\n");
+            return 0;
+        }
+    }
 
 int main() {
     int num1, num2, choice;
@@ -56,24 +72,3 @@ int main() {
     return 0;
 }
 
-
-int add(int a, int b) {
-    return a + b;
-}
-
-int subtract(int a, int b) {
-    return a - b;
-}
-
-int multiply(int a, int b) {
-    return a * b;
-}
-
-float divide(int a, int b) {
-    if (b != 0)
-        return (float)a / b;
-    else {
-        printf("Division by zero is not allowed.\n");
-        return 0;
-    }
-}
