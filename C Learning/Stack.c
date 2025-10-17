3f2f79fdb8f8aca7c0610fc5e9fefd6c1dea2f3c
Yash <yashm6711@gmail.com>
2025-10-18 00:49:29 +0530

    [Feature] Added report option in Menu

--- C Learning/Stack.c
+++ C Learning/Stack.c
@@ -10,58 +10,106 @@
 int stack[5];
 int top = -1;
 
-void push(Value) {          // push = insert
-    
-    if (top == 4) {
-        printf("Stack Overflow! %d.\n", Value);
-    } else {
-        stack[++top] = Value;
-        printf("%d inserted into the Stack", Value);
-    }
-}
+int totalInsert = 0;
+int totalDelete = 0;
 
-int pop(int size) {         // pop = delete
-    
+int Menu();
+
+
+    void push(int Value) {          // push = insert
+        if (top == 4) {
+            printf("Stack Overflow! %d cannot be inserted.\n", Value);
+        } else {
+            stack[++top] = Value;
+            totalInsert++;
+            printf("%d inserted into the Stack\n", Value);
+        }
+    }
+
+    int pop() {             // pop = delete
+        if (top == -1) {
+            printf("Stack is empty! Nothing to delete.\n");
+        return -1;
+    }
+        
     int popped = stack[top];
     top--;
-    printf("%d deleted from stack. \n", popped);
+    totalDelete++;
+    printf("%d deleted from stack.\n", popped);
     return popped;
-    
 }
 
-void show() {
-    
-    for (int i = top; i>=0; i--) {
-        printf("%d\n", stack[i]);
+    void show() {
+        if (top == -1) { // Empty check
+            printf("Stack is empty! No records to show.\n");
+            return;
+        }
         
+        printf("Stack elements are:\n");
+            for (int i = top; i >= 0; i--) {
+        printf("%d\n", stack[i]);
     }
 }
 
-int main () {
-    
-    int i, choices, Value;
-    int size = 5;
-    
-    printf("Enter %d Elements for the stack :\n ", size);
-    for (i = 0; i < size; i++) {
-        scanf("%d", &stack[i]);
-        top++; // top = -1 + 1 = 0
-        //stack[0++]
+void clearScr() {
+    for (int i = 0; i < 50; i++) {
+        printf("\n");
     }
+}
+
+int report() {
+    
+    int choices;
+    clearScr();
     
     do {
-        printf("\n--- MENU---\n");
+        printf("\n---Report---\n");
+        printf("1. Total Insert\n");
+        printf("2. Total Delete\n");
+        printf("3. Return Back\n");
+        
+        printf("Enter your choice: ");
+        scanf("%d", &choices);
+        
+        switch(choices) {
+            case 1:
+                printf("Total Insert: %d\n", totalInsert);
+                break;
+                
+            case 2:
+                printf("Total Delete: %d\n", totalDelete);
+                break;
+                
+            case 3:
+                printf("Returning Back...\n");
+                clearScr();
+                return 0;
+                
+            default:
+                printf("Invalid choice\n");
+                break;
+        }
+    } while (1);
+    
+}
+
+int Menu() {
+    
+    int choices, Value;
+    do {
+        printf("\n--- MENU ---\n");
         printf("1. Delete\n");
         printf("2. View\n");
         printf("3. Insert\n");
-        printf("4. Exit\n");
+        printf("4. Report\n");
+        printf("5. Exit\n");
         
         printf("Enter your choice: ");
         scanf("%d", &choices);
         
         switch (choices) {
             case 1:
-                pop(size);
+                pop();
                 break;
                 
             case 2:
@@ -75,14 +123,32 @@ int main () {
                 break;
                 
             case 4:
-                printf("Exiting Program \n");
+                report();
                 break;
                 
+            case 5:
+                printf("Exiting Program...\n");
+                return 0;
+                
             default:
-                printf("Invalid Choice !!!");
+                printf("Invalid Choice !!!\n");
         }
-        
-        
-    } while(choices != 4 );
+    } while (1);
+    
+}
+
+int main () {
+    int i, Value;
+    int size = 5;
+    
+    printf("Enter %d Elements for the stack:\n", size);
+    for (i = 0; i < size; i++) {
+        scanf("%d", &stack[i]);
+        top++;
+        totalInsert++;
+    }
+    
+    Menu();
+
     return 0;
 }
